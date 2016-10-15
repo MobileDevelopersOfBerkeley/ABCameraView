c1d7ea771a584c0b5a6321947c4d22cf4707b338
Mudit Mittal <muditmittal@calvisitor-10-105-139-231.calvisitor.1918.berkeley.edu>
2016-10-15 14:19:10 -0700

    fixed everything but flash

--- CameraCocoapod/CameraCocoapod/ViewController.swift
+++ CameraCocoapod/CameraCocoapod/ViewController.swift
@@ -18,6 +18,8 @@ class ViewController: UIViewController {
     @IBOutlet weak var takePhoto: UIButton!
     
     var buttons: [UIButton?] = []
+    var camera = true
+    var flash = false
     var topLeftButton: UIButton?
     var topMiddleButton: UIButton?
     var topRightButton: UIButton?
@@ -28,6 +30,7 @@ class ViewController: UIViewController {
     var captureSession: AVCaptureSession?
     var stillImageOutput: AVCaptureStillImageOutput?
     var previewLayer: AVCaptureVideoPreviewLayer?
+
     
     @IBAction func deleteImage(_ sender: AnyObject) {
         capturedImage.isHidden = true
@@ -41,6 +44,11 @@ class ViewController: UIViewController {
     }
     
     @IBAction func didPressTakePhoto(_ sender: AnyObject) {
+        if flash {
+            toggleFlash()
+            sleep(1)
+            toggleFlash()
+        }
         if let videoConnection = stillImageOutput?.connection(withMediaType: AVMediaTypeVideo) {
             videoConnection.videoOrientation = AVCaptureVideoOrientation.portrait
             stillImageOutput?.captureStillImageAsynchronously(from: videoConnection) {
@@ -49,8 +57,13 @@ class ViewController: UIViewController {
                 let imageData = AVCaptureStillImageOutput.jpegStillImageNSDataRepresentation(sampleBuffer)
                 let dataProvider = CGDataProvider(data: imageData as! CFData)
                 let cgImageRef = CGImage(jpegDataProviderSource: dataProvider!, decode: nil, shouldInterpolate: true, intent: CGColorRenderingIntent.defaultIntent)
-                let image = UIImage(cgImage: cgImageRef!, scale: 1.0, orientation: UIImageOrientation.right)
+                if (self.camera == true) {
+                    let image = UIImage(cgImage: cgImageRef!, scale: 1.0, orientation: UIImageOrientation.right)
                     self.capturedImage.image = image
+                } else {
+                    let image = UIImage(cgImage: cgImageRef!, scale: 1.0, orientation: .leftMirrored)
+                    self.capturedImage.image = image
+                }
                 }
                 self.capturedImage.isHidden = false
                 self.cancel.isHidden = false
@@ -63,24 +76,34 @@ class ViewController: UIViewController {
         }
     }
     
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        capturedImage.isHidden = true
-        cancel.isHidden = true
-        usePhoto.isHidden = true
-        
-        // Do any additional setup after loading the view, typically from a nib.
+    func changeCamera(sender: AnyObject?) {
+        camera = !camera
+        reloadCamera()
+        previewLayer!.frame = previewView.bounds
     }
     
-    override func viewWillAppear(_ animated: Bool) {
-        super.viewWillAppear(animated)
+    func reloadCamera() {
+        captureSession?.stopRunning()
         captureSession = AVCaptureSession()
+        previewLayer?.removeFromSuperlayer()
         captureSession!.sessionPreset = AVCaptureSessionPresetPhoto
-        let backCamera = AVCaptureDevice.defaultDevice(withMediaType: AVMediaTypeVideo)
-        var error: NSError?
-        var input: AVCaptureDeviceInput!
+        var captureDevice: AVCaptureDevice! = nil
+        if (camera == false) {
+            let videoDevices = AVCaptureDevice.devices(withMediaType: AVMediaTypeVideo)
+            for device in videoDevices!{
+                let device = device as! AVCaptureDevice
+                if device.position == AVCaptureDevicePosition.front {
+                    captureDevice = device
+                    break
+                }
+            }
+        } else {
+            captureDevice = AVCaptureDevice.defaultDevice(withMediaType: AVMediaTypeVideo)
+        }
+        var error : NSError?
+        var input : AVCaptureDeviceInput!
         do {
-            input = try AVCaptureDeviceInput(device: backCamera)
+            input = try AVCaptureDeviceInput(device: captureDevice)
         } catch let err as NSError {
             error = err
             input = nil
@@ -100,24 +123,69 @@ class ViewController: UIViewController {
         }
     }
     
+    func turnFlashOn(sender: UIButton) {
+        flash = !flash
+        if (flash == true) {
+            sender.setImage(UIImage(named: "flash-on-button"), for: UIControlState.normal)
+        } else {
+            sender.setImage(UIImage(named: "flash-off-button"), for: UIControlState.normal)
+        }
+       
+    }
+    
+    func toggleFlash() {
+        if (camera == true) {
+            let device = AVCaptureDevice.defaultDevice(withMediaType: AVMediaTypeVideo)
+            if (device?.hasTorch)! {
+                do {
+                    try device?.lockForConfiguration()
+                    device?.flashMode = .on
+                    if (device?.torchMode == AVCaptureTorchMode.on) {
+                        device?.torchMode = AVCaptureTorchMode.off
+                    } else {
+                        try device?.setTorchModeOnWithLevel(1.0)
+                    }
+                    device?.unlockForConfiguration()
+                } catch {
+                    print(error)
+                }
+            }
+        }
+        
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        capturedImage.isHidden = true
+        cancel.isHidden = true
+        usePhoto.isHidden = true
+        reloadCamera()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+    }
+    
     override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
         let screenSize = previewView.bounds.size
         if let touchPoint = touches.first {
             let x = touchPoint.location(in: previewView).y / screenSize.height
             let y = 1.0 - touchPoint.location(in: previewView).x / screenSize.width
             let focusPoint = CGPoint(x: x, y: y)
-            
-            if let device = AVCaptureDevice.defaultDevice(withMediaType: AVMediaTypeVideo) {
-                do {
-                    try device.lockForConfiguration()
-                    
-                    device.focusPointOfInterest = focusPoint
-                    device.focusMode = .autoFocus
-                    device.exposurePointOfInterest = focusPoint
-                    device.exposureMode = AVCaptureExposureMode.continuousAutoExposure
-                    device.unlockForConfiguration()
-                }
-                catch {
+            if (camera == true) {
+                if let device = AVCaptureDevice.defaultDevice(withMediaType: AVMediaTypeVideo) {
+                    do {
+                        try device.lockForConfiguration()
+                        
+                        device.focusPointOfInterest = focusPoint
+                        device.focusMode = .autoFocus
+                        device.exposurePointOfInterest = focusPoint
+                        device.exposureMode = AVCaptureExposureMode.continuousAutoExposure
+                        device.unlockForConfiguration()
+                    }
+                    catch {
+                    }
                 }
             }
         }
@@ -138,50 +206,57 @@ class ViewController: UIViewController {
         
         switch position {
         case .topLeft:
-            let topLeftButton = UIButton(frame: CGRect(x: Int(capturedImage.bounds.minX + 20), y: Int(capturedImage.bounds.minY + 30), width: 26, height: 26))
+            let topLeftButton = UIButton(frame: CGRect(x: Int(capturedImage.bounds.minX + 20), y: Int(capturedImage.bounds.minY + 30), width: 34, height: 34))
             topLeftButton.setImage(UIImage(named: imageName), for: UIControlState.normal)
+            topLeftButton.imageView?.contentMode = UIViewContentMode.scaleAspectFit
             self.view.addSubview(topLeftButton)
             buttons.append(topLeftButton)
             return topLeftButton
             
         case .topMiddle:
-            let topMiddleButton = UIButton(frame: CGRect(x: Int((capturedImage.frame.size.width / 2) - 13), y: Int(capturedImage.bounds.minY + 30), width: 26, height: 26))
+            let topMiddleButton = UIButton(frame: CGRect(x: Int((capturedImage.frame.size.width / 2) - 17), y: Int(capturedImage.bounds.minY + 30), width: 34, height: 34))
             topMiddleButton.setImage(UIImage(named: imageName), for: UIControlState.normal)
             self.view.addSubview(topMiddleButton)
+            topMiddleButton.imageView?.contentMode = UIViewContentMode.scaleAspectFit
             buttons.append(topMiddleButton)
             return topMiddleButton
 
         case .topRight:
-            let topRightButton = UIButton(frame: CGRect(x: Int(capturedImage.bounds.maxX - 46) , y: Int(capturedImage.bounds.minY + 30), width: 26, height: 26))
+            let topRightButton = UIButton(frame: CGRect(x: Int(capturedImage.bounds.maxX - 54) , y: Int(capturedImage.bounds.minY + 30), width: 34, height: 34))
             topRightButton.setImage(UIImage(named: imageName), for: UIControlState.normal)
+            topRightButton.imageView?.contentMode = UIViewContentMode.scaleAspectFit
             self.view.addSubview(topRightButton)
             buttons.append(topRightButton)
             return topRightButton
             
         case .middleLeft:
-            let middleLeftButton = UIButton(frame: CGRect(x: Int(capturedImage.bounds.minX + 20), y: Int((capturedImage.frame.size.height / 2) - 13) , width: 26, height: 26))
+            let middleLeftButton = UIButton(frame: CGRect(x: Int(capturedImage.bounds.minX + 20), y: Int((capturedImage.frame.size.height / 2) - 17) , width: 34, height: 34))
             middleLeftButton.setImage(UIImage(named: imageName), for: UIControlState.normal)
+            middleLeftButton.imageView?.contentMode = UIViewContentMode.scaleAspectFit
             self.view.addSubview(middleLeftButton)
             buttons.append(middleLeftButton)
             return middleLeftButton
             
         case .middleRight:
-            let middleRightButton = UIButton(frame: CGRect(x: Int(capturedImage.bounds.maxX - 46), y: Int((capturedImage.frame.size.height / 2) - 13) , width: 26, height: 26))
+            let middleRightButton = UIButton(frame: CGRect(x: Int(capturedImage.bounds.maxX - 54), y: Int((capturedImage.frame.size.height / 2) - 17) , width: 34, height: 34))
             middleRightButton.setImage(UIImage(named: imageName), for: UIControlState.normal)
+            middleRightButton.imageView?.contentMode = UIViewContentMode.scaleAspectFit
             self.view.addSubview(middleRightButton)
             buttons.append(middleRightButton)
             return middleRightButton
             
         case .bottomLeft:
-            let bottomLeftButton = UIButton(frame: CGRect(x: Int(capturedImage.bounds.minX + 20), y: Int(capturedImage.bounds.maxY - 56) , width: 26, height: 26))
+            let bottomLeftButton = UIButton(frame: CGRect(x: Int(capturedImage.bounds.minX + 20), y: Int(capturedImage.bounds.maxY - 64) , width: 34, height: 34))
             bottomLeftButton.setImage(UIImage(named: imageName), for: UIControlState.normal)
+            bottomLeftButton.imageView?.contentMode = UIViewContentMode.scaleAspectFit
             self.view.addSubview(bottomLeftButton)
             buttons.append(bottomLeftButton)
             return bottomLeftButton
             
         case .bottomRight:
-            let bottomRightButton = UIButton(frame: CGRect(x: Int(capturedImage.bounds.maxX - 46), y: Int(capturedImage.bounds.maxY - 56) , width: 26, height: 26))
+            let bottomRightButton = UIButton(frame: CGRect(x: Int(capturedImage.bounds.maxX - 54), y: Int(capturedImage.bounds.maxY - 64) , width: 34, height: 34))
             bottomRightButton.setImage(UIImage(named: imageName), for: UIControlState.normal)
+            bottomRightButton.imageView?.contentMode = UIViewContentMode.scaleAspectFit
             self.view.addSubview(bottomRightButton)
             buttons.append(bottomRightButton)
             return bottomRightButton
@@ -192,14 +267,31 @@ class ViewController: UIViewController {
     override func viewDidAppear(_ animated: Bool) {
         super.viewDidAppear(animated)
         previewLayer!.frame = previewView.bounds
-        print(previewView.bounds)
-        topMiddleButton = addButton(Position.topMiddle, imageName: "cancel-button")
-        topRightButton = addButton(Position.topRight, imageName: "cancel-button")
+        
+        // How to add a button
+        //topMiddleButton = addButton(Position.topMiddle, imageName: "cancel-button")
+        
+        // If user wants to add a Flash Button:
+        //topRightButton = addButton(Position.topRight, imageName: "flash-button")
+        // topRightButton.addTarget(self, action: "flashToggle:", for: UIControlEvents.touchUpInside)
+        
+        // If user wants to add a Switch Camera Button:
+        //bottomRightButton = addButton(Position.topRight, imageName: "flash-button")
+        // bottomRightButton.addTarget(self, action: "switchCamera:", for: UIControlEvents.touchUpInside)
+        
+        topLeftButton = addButton(Position.topLeft, imageName: "flash-off-button")
+        topLeftButton?.addTarget(self, action: #selector(turnFlashOn), for: UIControlEvents.touchUpInside)
+        topRightButton = addButton(Position.topRight, imageName: "switch-camera-button")
+        topRightButton?.addTarget(self, action: #selector(changeCamera), for: UIControlEvents.touchUpInside)
+        
+        
+        /*topRightButton = addButton(Position.topRight, imageName: "cancel-button")
         topLeftButton = addButton(Position.topLeft, imageName: "cancel-button")
+         
         leftMiddleButton = addButton(Position.middleLeft, imageName: "cancel-button")
         rightMiddleButton = addButton(Position.middleRight, imageName: "cancel-button")
         bottomLeftButton = addButton(Position.bottomLeft, imageName: "cancel-button")
-        bottomRightButton = addButton(Position.bottomRight, imageName: "cancel-button")
+        bottomRightButton = addButton(Position.bottomRight, imageName: "cancel-button")*/
     }
     
     override func didReceiveMemoryWarning() {
